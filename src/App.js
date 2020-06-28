d273545bf2f5c4a2141cbaa6d7d031f436ae7c59
Shiiho <48092336+vinhan2020@users.noreply.github.com>
2020-06-28 15:06:01 +0700

    new App js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Home from './component/Home'
 import Login from './component/Login'
+import HangSX from './component/input/hangsx'
+import Congty from './component/input/congty'
+import Sanpham from './component/input/sanpham'
+import Khachhang from './component/input/khachhang'
+
+
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path={'/login'} component={Login} />
+        <Route path={'/hangsx'} component={HangSX} />
+        <Route path={'/congty'} component={Congty} />
+
+        <Route path={'/sanpham'} component={Sanpham} />
+
+        <Route path={'/khachhang'} component={Khachhang} />
 
         <Route path={'/'} component={Home} />
       </Switch>
